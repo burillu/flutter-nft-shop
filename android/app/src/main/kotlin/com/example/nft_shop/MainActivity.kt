b284075bea6e78289d32f953467fabb777cb77e0
burillu <burillu90@hotmail.it>
2024-08-21 17:25:29 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/nft_shop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nft_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:nft_shop/app.dart';
+
+void main() {
+  runApp(App());
+}

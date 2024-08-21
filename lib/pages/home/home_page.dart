429d39caf0554a8cef1d32ef1fbc454725b90295
burillu <burillu90@hotmail.it>
2024-08-21 18:07:41 +0200

    Tab bar layout ok

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -3,6 +3,38 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "NFT Shop",
+          ),
+          bottom: TabBar(
+            tabs: [
+              Row(
+                children: [
+                  Icon(Icons.home),
+                  Text("home"),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.person),
+                  Text("Profilo"),
+                ],
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Text("Hello Home"),
+            Text("Hello Profile"),
+          ],
+        ),
+      ),
+    );
   }
 }
